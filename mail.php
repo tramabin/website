c0f4018fd83c778dde808e9dc908bf9926f2806d
tramabin <tramabin@student.wethinkcode.co.za>
2022-08-12 12:42:31 +0200

    Complete website

--- mail.php
+++ mail.php
@@ -1,25 +1,21 @@
 <?php
 //get data from form 
-if (isset($_POST['submit']) ) {
-    
-    $name = $_POST['name'];
-    $city= $_POST['city'];
-    $serviceses = $_POST['serviceses'];
-    $contact= $_POST['contact'];
-    $email= $_POST['email'];
-    $message= $_POST['message'];
-    $to = "tokollotk818@mail.com";
-    $subject = "Mail From The Dlamini Property Group";
-    $txt ="Name = ". $name . "\r\n  City = " . $city .
-    "\r\n  Serviceses = " . $serviceses . "\r\n  Contact = " . $contact .
-    "\r\n  Email = " . $email . "\r\n Message =" . $message;
-    $headers = "From: noreply@yoursite.com" . "\r\n" .
-    "CC: somebodyelse@example.com";
-    if ($email!=null ) {
-        mail($to, $subject, $txt, $headers);
-    }
-    //redirect
-    header("Location:index.html");
 
+$name = $_POST['name'];
+$city= $_POST['city'];
+$services = $_POST['services'];
+$contact= $_POST['contact'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "tokollo@thedlaminipropertygroup.com";
+$subject = "Client Enquiry";
+$txt ="Name = ". $name . "\r\n  City = " . $city .
+"\r\n  services = " . $services . "\r\n  Contact = " . $contact .
+"\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@thedlaminipropertygroup.com";
+if ($email!=null ) {
+    mail($to, $subject, $txt, $headers);
 }
+//redirect
+header("Location:index.html");
 ?>
